7179afbae8ea44cf372933ba98f8856ba920ad0d
perez.e <perez.e@softlab.umet.edu.ar>
2022-11-09 12:36:27 -0300

    feat: comments

--- modules/webapi/postgres.tf
+++ modules/webapi/postgres.tf
@@ -27,6 +27,11 @@ resource "azurerm_postgresql_flexible_server_database" "webapi" {
   charset   = "utf8"
 }
 
+# SELECT * FROM pg_available_extensions order by name;
+# CREATE EXTENSION IF NOT EXISTS tablefunc
+# CREATE EXTENSION IF NOT EXISTS unaccent
+# CREATE EXTENSION IF NOT EXISTS uuid-ossp
+
 resource "azurerm_postgresql_flexible_server_configuration" "webapi01" {
   name      = "azure.extensions"
   server_id = azurerm_postgresql_flexible_server.webapi01.id
