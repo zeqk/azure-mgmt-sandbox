4b24d2c90a0f534b226fb06b16e16ddfc64e33aa
perez.e <perez.e@softlab.umet.edu.ar>
2022-11-25 11:02:14 -0300

    feat: pg_read_all_data

--- modules/webapi/psql-roles.tf
+++ modules/webapi/psql-roles.tf
@@ -8,4 +8,19 @@ resource "postgresql_role" "webapi" {
   name     = "webapi"
   login    = true
   password = random_password.webapi.result
+  lifecycle {
+    ignore_changes = [
+      roles,
+    ]
+  }
+}
+
+resource "postgresql_grant_role" "webapi_read" {
+  role       = "webapi"
+  grant_role = "pg_read_all_data"
+}
+
+resource "postgresql_grant_role" "webapi_write" {
+  role       = "webapi"
+  grant_role = "pg_write_all_data"
 }
